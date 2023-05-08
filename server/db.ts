05b9618fffa5dcf5e1930445c432ca096e1741aa
Mark <mdoghram0@gmail.com>
2023-05-08 13:44:48 -0400

    Attempting to add API function. Currently have issue with loaders.

---
+++ server/db.ts
@@ -0,0 +1,17 @@
+export const MockData = [
+    {
+        studentId: "123",
+        studentName: "John Doe",
+        courseId: "se577"
+    },
+    {
+        studentId: "456",
+        studentName: "Jane Doe",
+        courseId: "se577"
+    },
+    {
+        studentId: "789",
+        studentName: "Tim Doe",
+        courseId: "se577"
+    }
+];
---
+++ wrath-and-glory-pages/src/pages/ApiInterfaces.ts
@@ -0,0 +1,5 @@
+export interface StudentApiInterface {
+    studentId: string,
+    studentName: string,
+    courseId: string
+}
